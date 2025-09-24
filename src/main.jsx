02c55e498866c05d3af0d766063e1682dbd4d877
AndreaNardinocchi <andrea.nardinocchi76@gmail.com>
2025-09-24 19:31:35 +0100

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { Provider } from 'react-redux'
+// import store from './store.js'
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//     <App />
+//     </Provider>
+//   </React.StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
+import { BrowserRouter } from "react-router-dom"; // ← import this
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <BrowserRouter basename="/e-plantShopping">
+        <App />
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
